c2375582c8a62dbc062a1efaa86d795bed79f09a
ArthurSalt <arthursalt1990@gmail.com>
2024-10-11 17:51:55 +0300

    thunk done

--- src/asyncAction/customers.js
+++ src/asyncAction/customers.js
@@ -1,11 +1,12 @@
-
 import { addManyCustomersAction } from "../store/customerReduser"
 
 export const fetchCustomers = () => {
-    
     return dispatch =>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(json => {dispatch(addManyCustomersAction(json))})
+        .then(json => {
+            console.log(json)
+            return  dispatch(addManyCustomersAction(json))
+        })
     }
 }
